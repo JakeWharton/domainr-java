a490a35920b3e6800ac497408b7de69cca5202ed
Jake Wharton <jw@squareup.com>
2013-09-23 23:06:45 -0700

    Depreacte in favor of Retrofit.

--- src/main/java/com/jakewharton/domainr/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Classes which assist in marshalling the native builder methods to proper
- * remote API calls and the resulting JSON to POJOs specifically for the
- * Domainr API.
- */
-package com.jakewharton.domainr;
